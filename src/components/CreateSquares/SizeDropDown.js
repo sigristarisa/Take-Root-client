6aa986d6d1e98f3c5ef4cdcfe54b41834fb3c11c
sigristarisa <arisasigrist.ch@gmail.com>
2022-07-28 09:49:11 +0200

    changed SquaresCalculator to dropdown

---
+++ src/components/CreateSquares/SizeDropDown.js
@@ -0,0 +1,22 @@
+const SizeDropDown = ({ label, name, onChange }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <select id={name} name={name} onChange={onChange}>
+        <option>...</option>
+        <option value='1'>~ 30cm / 1foot</option>
+        <option value='2'>~ 60cm / 2feet</option>
+        <option value='3'>~ 90cm / 3feet</option>
+        <option value='4'>~ 120cm / 4feet</option>
+        <option value='5'>~ 150cm / 5feet</option>
+        <option value='6'>~ 180cm / 6feet</option>
+        <option value='7'>~ 210cm / 7feet</option>
+        <option value='8'>~ 240cm / 8feet</option>
+        <option value='9'>~ 270cm / 9feet</option>
+        <option value='10'>~ 300cm / 10feet</option>
+      </select>
+    </div>
+  );
+};
+
+export default SizeDropDown;
--- src/components/CreateSquares/SquaresCalculator.js
+++ src/components/CreateSquares/SquaresCalculator.js
@@ -1,42 +1,24 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { userContext, squareContext } from "../../helpers/createContext";
+import SizeDropDown from "./SizeDropDown";
 import client from "../../helpers/client";
-import FormInput from "../Signup/FormInput";
 
 const SquaresCalculator = () => {
   const { user } = useContext(userContext);
   const { square, setSquare } = useContext(squareContext);
-  console.log("whats user: ", user);
-  const [size, setSize] = useState({ length: 0, height: 0, unit: "" });
 
-  const calculateSquares = (length, height, unit) => {
-    let row;
-    let column;
-    if (unit === "cm") {
-      row = Math.floor(height / 30);
-      column = Math.floor(length / 30);
-    }
-    if (unit === "feet") {
-      row = Math.floor(height);
-      column = Math.floor(length);
-    }
-    setSquare({ row, column });
-  };
-
-  const handleSize = (e) => {
+  const handleSquares = (e) => {
     const { name, value } = e.target;
-    setSize({ ...size, [name]: value });
+    setSquare({ ...square, [name]: value });
   };
 
-  const submitSize = (e) => {
+  console.log("squares: ", square);
+
+  const submitRaisedBed = (e) => {
     e.preventDefault();
-    calculateSquares(size.length, size.height, size.unit);
-  };
-
-  const submitRaisedBed = () => {
     client
       .post("/raisedbed", { ...square, userId: user.id })
-      .then((res) => console.log("whats this: ", res.data));
+      .then((res) => console.log("response: ", res.data));
   };
 
   return (
@@ -46,31 +28,19 @@ const SquaresCalculator = () => {
         Choose the size that is the closest to your raised bed. As we are
         working with the companion method, one square will be around 30cm/1foot.{" "}
       </p>
-      <form onSubmit={submitSize}>
-        <FormInput
-          label='Length'
-          name='length'
-          type='number'
-          value={size.length}
-          placeholder='100'
-          handleChange={handleSize}
+      <form onSubmit={submitRaisedBed}>
+        <SizeDropDown
+          label={"Length: "}
+          name={"column"}
+          onChange={handleSquares}
         />
-        <FormInput
-          label='Height'
-          name='height'
-          type='number'
-          value={size.height}
-          placeholder='100'
-          handleChange={handleSize}
+        <SizeDropDown
+          label={"Height: "}
+          name={"row"}
+          onChange={handleSquares}
         />
-        <select name='unit' onChange={handleSize}>
-          <option>...</option>
-          <option value='cm'>cm</option>
-          <option value='feet'>feet</option>
-        </select>
-        <input type='submit' value='Preview' />
+        <input type='submit' value='Next Step' />
       </form>
-      <button onClick={() => submitRaisedBed()}>Next Step</button>
     </section>
   );
 };
