dd8fa35f0ae4ecd43f22bfc967eeb900335ece59
sigristarisa <arisasigrist.ch@gmail.com>
2022-08-01 10:36:43 +0200

    able to delete all plants

--- src/components/DesignRaisedBed/ButtonBar.js
+++ src/components/DesignRaisedBed/ButtonBar.js
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import client from "../../helpers/client";
+import { raisedBedContext } from "../../helpers/createContext";
+
 const ButtonBar = () => {
+  const { raisedBed, setRaisedBed } = useContext(raisedBedContext);
+
+  const deleteSquares = () => {
+    client
+      .patch(`/raisedBed/${raisedBed.raisedBed.id}`, "")
+      .then((res) => setRaisedBed(res.data));
+  };
+
   return (
     <div>
-      <button>Delete All</button>
+      <button onClick={() => deleteSquares()}>Delete All</button>
       <button>Save</button>
     </div>
   );
