9d5674f4646367bbd0ca737bdb589b6c2809d758
sigristarisa <arisasigrist.ch@gmail.com>
2023-07-18 11:22:03 +0200

    installed eslint plugin cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/home_page.cy.js
@@ -0,0 +1,5 @@
+describe("The Home Page", () => {
+  it("successfully load", () => {
+    cy.visit("/");
+  });
+});
